6ee1dafaf8f8afbb7b6105080d7ef040e6796ccb
haru864 <kawachigenji95@gmail.com>
2022-05-31 00:11:35 +0900

    Data Structure I Day 6 String

--- Algorithms/Java/242.valid-anagram.java
+++ Algorithms/Java/242.valid-anagram.java
@@ -11,14 +11,32 @@ class Solution {
 
     public boolean isAnagram(String s, String t) {
 
-        return sortString(s).equals(sortString(t));
+        if (s.length() != t.length())
+            return false;
+
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            arr1[s.charAt(i) - 'a']++;
+            arr2[t.charAt(i) - 'a']++;
+        }
+        printArr(arr1);
+        printArr(arr2);
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i])
+                return false;
+        }
+
+        return true;
     }
 
-    public String sortString(String s) {
-
-        char[] arr = s.toCharArray();
-        Arrays.sort(arr);
-        return new String(arr);
+    public void printArr(int[] arr) {
+        for (var n : arr) {
+            System.out.print(n);
+        }
+        System.out.println("\n");
     }
 }
 // @lc code=end
--- Algorithms/Java/383.ransom-note.java
+++ Algorithms/Java/383.ransom-note.java
@@ -10,24 +10,22 @@ import javax.print.attribute.HashAttributeSet;
 
 // @lc code=start
 class Solution {
+
     public boolean canConstruct(String ransomNote, String magazine) {
+
         HashMap<Character, Integer> map = new HashMap<>();
+
         for (int i = 0; i < magazine.length(); i++) {
-            var c = magazine.charAt(i);
-            if (map.get(c) == null) {
-                map.put(c, 1);
-            } else {
-                map.put(c, map.get(c) + 1);
-            }
+            char ch = magazine.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
         for (int i = 0; i < ransomNote.length(); i++) {
-            var c = ransomNote.charAt(i);
-            if (map.get(c) == null || map.get(c) <= 0) {
+            char ch = ransomNote.charAt(i);
+            if (map.get(ch) == null || map.get(ch) < 1) {
                 return false;
-            } else {
-                map.put(c, map.get(c) - 1);
             }
+            map.put(ch, map.getOrDefault(ch, 0) - 1);
         }
 
         return true;

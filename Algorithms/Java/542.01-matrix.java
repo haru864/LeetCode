ac38b35227d2e495b774685b49a20f5418d1cc7b
haru864 <kawachigenji95@gmail.com>
2022-06-07 01:35:09 +0900

    Algorithm I Day 9 Breadth-First Search / Depth-First Search

--- Algorithms/Java/542.01-matrix.java
+++ Algorithms/Java/542.01-matrix.java
@@ -1,3 +1,7 @@
+import java.security.SecureRandom;
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.CountDownLatch;
 
 /*
@@ -9,38 +13,42 @@ import java.util.concurrent.CountDownLatch;
 // @lc code=start
 class Solution {
 
+    int[] ROWS = new int[] { -1, 0, 0, 1 };
+    int[] COLS = new int[] { 0, -1, 1, 0 };
+
     public int[][] updateMatrix(int[][] mat) {
 
-        int row = mat.length;
-        int column = mat[0].length;
-        int[][] ans = new int[row][column];
+        int height = mat.length;
+        int width = mat[0].length;
+        int[][] dist = new int[height][width];
+        Queue<List<Integer>> q = new ArrayDeque<>();
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                // System.out.println(i + " " + j + " " + ans[i][j]);
-                if (mat[i][j] == 0)
-                    continue;
-                ans[i][j] = updateMatrixHelper(mat, 0, i, j);
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                    q.add(Arrays.asList(i, j));
+                } else {
+                    dist[i][j] = Integer.MAX_VALUE;
+                }
             }
         }
 
-        return ans;
-    }
-
-    public int updateMatrixHelper(int[][] mat, int dist, int r, int c) {
-
-        if (r < 0 || r >= mat.length || c < 0 || c >= mat[0].length || dist > 10) {
-            return Integer.MAX_VALUE;
-        } else if (mat[r][c] == 0) {
-            return dist;
+        while (!q.isEmpty()) {
+            List<Integer> currMass = q.remove();
+            for (int i = 0; i < 4; i++) {
+                int nextRow = currMass.get(0) + ROWS[i];
+                int nextCol = currMass.get(1) + COLS[i];
+                if (nextCol >= 0 && nextCol < width && nextRow >= 0 && nextRow < height) {
+                    if (dist[nextRow][nextCol] > dist[currMass.get(0)][currMass.get(1)] + 1) {
+                        dist[nextRow][nextCol] = dist[currMass.get(0)][currMass.get(1)] + 1;
+                        q.add(Arrays.asList(nextRow, nextCol));
+                    }
+                }
+            }
         }
 
-        int ret = updateMatrixHelper(mat, dist + 1, r - 1, c);
-        ret = Math.min(ret, updateMatrixHelper(mat, dist + 1, r + 1, c));
-        ret = Math.min(ret, updateMatrixHelper(mat, dist + 1, r, c - 1));
-        ret = Math.min(ret, updateMatrixHelper(mat, dist + 1, r, c + 1));
-
-        return ret;
+        return dist;
     }
 }
 // @lc code=end
--- Algorithms/Java/994.rotting-oranges.java
+++ Algorithms/Java/994.rotting-oranges.java
@@ -1,4 +1,8 @@
 import java.nio.BufferOverflowException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
 /*
  * @lc app=leetcode id=994 lang=java
@@ -9,28 +13,60 @@ import java.nio.BufferOverflowException;
 // @lc code=start
 class Solution {
 
+    int[] ROWS = new int[] { -1, 0, 0, 1 };
+    int[] COLS = new int[] { 0, -1, 1, 0 };
+    int HEIGHT;
+    int WIDTH;
+
     public int orangesRotting(int[][] grid) {
 
-        return orangesRottingHelper(grid, 0);
-    }
-
-    public int orangesRottingHelper(int[][] grid, int count) {
-
-        if (!hasFresh(grid))
-            return count;
-
-    }
-
-    public boolean hasFresh(int[][] grid) {
+        Queue<List<Integer>> q = new ArrayDeque<>();
+        int freshOranges = 0;
+        this.HEIGHT = grid.length;
+        this.WIDTH = grid[0].length;
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1)
-                    return ture;
+                    freshOranges++;
+                else if (grid[i][j] == 2)
+                    q.add(new ArrayList<>(Arrays.asList(i, j)));
+            }
+        }
+        q.add(new ArrayList<>(Arrays.asList(-1, -1)));
+        // System.out.println("freshOranges: " + freshOranges + ", Queue_SIZE: " +
+        // q.size());
+
+        int count = 0;
+        while (q.size() > 0) {
+            List<Integer> currMass = q.remove();
+            // System.out.println(freshOranges + " " + currMass.get(0) + " " +
+            // currMass.get(1));
+            if (currMass.get(0) == -1) {
+                if (q.size() == 0) {
+                    if (freshOranges == 0)
+                        break;
+                    else
+                        return -1;
+                }
+                count++;
+                q.add(new ArrayList<>(Arrays.asList(-1, -1)));
+                continue;
+            }
+            for (int i = 0; i < 4; i++) {
+                int nextRow = currMass.get(0) + ROWS[i];
+                int nextCol = currMass.get(1) + COLS[i];
+                if (nextRow >= 0 && nextRow < HEIGHT && nextCol >= 0 && nextCol < WIDTH
+                        && grid[nextRow][nextCol] == 1) {
+                    freshOranges--;
+                    q.add(new ArrayList<>(Arrays.asList(nextRow, nextCol)));
+                    grid[nextRow][nextCol] = 2;
+                    // System.out.println("add " + nextRow + " " + nextCol + " " + freshOranges);
+                }
             }
         }
 
-        return false;
+        return count;
     }
 }
 // @lc code=end

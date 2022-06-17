430859a8b375ed3168379d7a5be6f05873b0cf12
haru864 <kawachigenji95@gmail.com>
2022-06-17 23:38:39 +0900

    Programming Skills II Day 7

--- Algorithms/Java/48.rotate-image.java
+++ Algorithms/Java/48.rotate-image.java
@@ -8,9 +8,23 @@
 class Solution {
 
     public void rotate(int[][] matrix) {
-        
-        
+
+        int len = matrix.length;
+
+        for (int r = 0; r < len / 2; r++) {
+            // System.out.println(r);
+            for (int c = r; c < len - 1 - r; c++) {
+                // System.out.println(r + " " + c);
+                int temp = matrix[len - 1 - c][r];
+                // System.out.println(temp);
+
+                matrix[len - 1 - c][r] = matrix[len - 1 - r][len - 1 - c];
+                matrix[len - 1 - r][len - 1 - c] = matrix[c][len - 1 - r];
+                matrix[c][len - 1 - r] = matrix[r][c];
+
+                matrix[r][c] = temp;
+            }
+        }
     }
 }
 // @lc code=end
-
